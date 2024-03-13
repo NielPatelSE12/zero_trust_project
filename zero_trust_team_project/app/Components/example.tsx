9bb27811ee5790248e9f3722e07e017ee0e761bd
markschwartz <markaschz@gmail.com>
2024-03-13 10:16:22 -0400

    learning git

---
+++ zero_trust_team_project/app/Components/example.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Example(){
+    return (
+        <div>lfshlfhl</div>
+    )
+}
--- zero_trust_team_project/app/layout.tsx
+++ zero_trust_team_project/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Example from "./Components/example";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>{children}</body>
       <h1>Testing first commit by Mark (again)</h1>
+      <Example />
     </html>
   );
 }
