466cdedc35ed1a3720f7fbb7d275e6ef9a34d92f
Mark Schwartz <markaschz@gmail.com>
2024-03-13 01:12:41 -0400

    Sidebar just to have something

--- zero_trust_team_project/app/layout.tsx
+++ zero_trust_team_project/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Sidebar from "./Components/SidebarFolder/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <h1>Testing first commit by Mark</h1>
+      <Sidebar />
     </html>
   );
 }
