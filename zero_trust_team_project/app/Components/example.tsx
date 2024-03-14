325508a94b10b98b7ba857dd9e468d4cf3284853
NielPatelSE12 <154777543+NielPatelSE12@users.noreply.github.com>
2024-03-14 17:25:22 -0400

    This is a commit

--- zero_trust_team_project/app/Components/example.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Example(){
-    return (
-        <div>lfshlfhl</div>
-    )
-}
--- zero_trust_team_project/app/layout.tsx
+++ zero_trust_team_project/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "./Components/SidebarFolder/sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
-      <Sidebar />
     </html>
   );
 }
