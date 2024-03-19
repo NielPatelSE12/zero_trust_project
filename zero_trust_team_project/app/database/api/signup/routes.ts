fc261bf32e815667a4d66eee21c68f1aa00892c7
NielPatelSE12 <154777543+NielPatelSE12@users.noreply.github.com>
2024-03-19 11:57:26 -0400

    database

--- zero_trust_team_project/app/database/api/signup/routes.ts
+++ zero_trust_team_project/app/database/api/signup/routes.ts
@@ -1,1 +1,8 @@
 import { NextResponse } from "next/server";
+import db from  "../../configure/db";
+
+try{
+    const results = await new Promise((resolve, reject) => {
+        db.query("SELECT * FROM credentials",(err:any,results:))
+    })
+}
