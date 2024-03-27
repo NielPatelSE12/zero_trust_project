4d029a8a730bd3fb43cd9168818a5d95f4e572e5
Brian Zapata Resendiz <brianzapata.0723@gmail.com>
2024-03-26 21:09:39 -0400

    renamed file and added edit func & working json

---
+++ app/drones/getDroneData.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import droneDatas from "./droneData.json";
+
+const GetDroneData = droneDatas;
+
+export default GetDroneData;
+/* const DroneData = [
+  {
+    Name: "This is Drone 1",
+    Model: "Model A",
+    Make: "Maker 1",
+    SerialNumber: "123",
+    BatteryLife: "100",
+    TopSpeed: "40",
+  },
+  {
+    Name: "This is Drone 2",
+    Model: "Model B",
+    Make: "Maker 2",g
+    SerialNumber: "456",
+    BatteryLife: "99",
+    DroneTopSpeed: "35",
+  },
+  {
+    Name: "This is Drone 3",
+    Model: "Model C",
+    Make: "Maker 1",
+    SerialNumber: "789",
+    BatteryLife: "10",
+    TopSpeed: "25",
+  },
+];
+
+export default DroneData;
+*/
