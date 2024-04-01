ce1aec407452cde35cf711eba5acb3e8f9a18fdf
NielPatelSE12 <154777543+NielPatelSE12@users.noreply.github.com>
2024-04-01 15:19:34 -0400

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,12 +2,11 @@
 import React from "react";
 import { useState } from "react";
 import './login.css';
-import dronify from './dronify.png';
-import Link from "next/link";
-const Login: React.FC = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+import dronify from './dronify.png'
 
+const Login: React.FC = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <div className="login-block">
             <div className="welcome">
@@ -15,15 +14,15 @@ const Login: React.FC = () => {
                 <img src={dronify.src} className="dronify"/>
             </div>
             <div className="input-block">
-                <input type="text" placeholder="Username" className="username" onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                <input type="text" placeholder="Username" className="username" onChange={(e) => {
                     setUsername(e.target.value);
                 }}></input>
                 <br></br>
-                <input type="password" placeholder="Password" className="password" onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                <input type="password" placeholder="Password" className="password" onChange={(e) => {
                     setPassword(e.target.value)
                 }}></input>
                 <br></br>
-                <Link className="login" href="/login">Login</Link>
+                <button className="login">Login</button>
                 <br></br>
                 <div className="sign-up">
                     First time? Sign up <a href="/signup" className="here">here</a>
