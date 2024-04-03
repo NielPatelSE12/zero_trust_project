aa90377e1585b5f58a566c9adb9295631e14f3b0
NielPatelSE12 <154777543+NielPatelSE12@users.noreply.github.com>
2024-04-02 22:11:41 -0400

    Update page.tsx

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -3,10 +3,29 @@ import React from "react";
 import { useState } from "react";
 import './login.css';
 import dronify from './dronify.png'
+import { useRouter } from "next/navigation";
 
 const Login: React.FC = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+    const router = useRouter();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = async () => {
+        await fetch('http://localhost:5000/login', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password})
+        }).then(response => response.json().then(
+            thing => {
+                if (thing === 'valid'){
+                    router.push('/location')
+                }
+            else{
+                router.push('/signup')
+            }}
+                ))
+    }
+
     return (
         <div className="login-block">
             <div className="welcome">
@@ -22,7 +41,9 @@ const Login: React.FC = () => {
                     setPassword(e.target.value)
                 }}></input>
                 <br></br>
-                <button className="login">Login</button>
+                <button className="login" onClick={() => {
+                    handleLogin();
+                }}>Login</button>
                 <br></br>
                 <div className="sign-up">
                     First time? Sign up <a href="/signup" className="here">here</a>
