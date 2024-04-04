3391d8d562ad935884f2fc935ca39e6413503d8d
NielPatelSE12 <154777543+NielPatelSE12@users.noreply.github.com>
2024-04-04 18:57:06 -0400

    homepage adjustment

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
-"use client";
 import Image from "next/image";
-import Heading from "./drones/heading"
+import Heading from "./drones/heading";
 import './home.css';
+import backgroundImage from "./home.jpeg";
 
 export default function Home() {
   return (
-<div>
-	<Heading/>
-	<div className="welcome-block">
-		<a className = "text-9xl text-gray-200 px-30 font-bold">
-			<p className = "text-center mt-3 mb-5 text-outline">Welcome to Dronify</p>
-		</a>
-		<a className = "text-5xl text-gray-200 px-30 font-bold">
-			<p className = "text-center text-outline">Leading the world in secure drone package delivery</p>
-		</a>
-	</div>
-</div>
-  )
+    <div className="background">
+      <Image
+        src={backgroundImage}
+        alt="Background Image"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="background-image"
+      />
+      <Heading />
+      <div className="welcome-block">
+        <h1 className="text-4xl lg:text-5xl text-center text-gray-900 font-bold mb-6">Welcome to Dronify</h1>
+        <p className="text-lg lg:text-xl text-center text-gray-700">Leading the world in secure drone package delivery</p>
+      </div>
+    </div>
+  );
 }
