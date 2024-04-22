015cceae6eb2edd55bd62e98a516eb609dfc0ecd
markschwartz <markaschz@gmail.com>
2024-04-22 11:05:46 -0400

    Changing schema to relate drones and users table

--- prisma/migrations/20240416172353_init_location/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Location" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "locationName" TEXT NOT NULL,
-    "locationCords" TEXT NOT NULL,
-    "locationAlg" TEXT NOT NULL
-);
